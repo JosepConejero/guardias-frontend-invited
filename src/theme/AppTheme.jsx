8d7c34b916d2ff689a095f0fe21e281175412fc9
JosepConejero <bermeu@gmail.com>
2023-11-01 13:58:30 +0100

    Removed comments from AppTheme.jsx

--- src/theme/AppTheme.jsx
+++ src/theme/AppTheme.jsx
@@ -6,7 +6,6 @@ import { purpleTheme } from "./";
 export const AppTheme = ({ children }) => {
   return (
     <ThemeProvider theme={purpleTheme}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
       {children}
     </ThemeProvider>
