d0eaca392152c3c94c8cd52f5cc01364d0babe2a
JosepConejero <bermeu@gmail.com>
2023-11-01 14:27:14 +0100

    Added lines to index.js in helpers folder

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -8,3 +8,6 @@ export * from "./extractItemByProperty";
 export * from "./dateComparator";
 export * from "./dayOfWeek";
 export * from "./uniqueKey";
+export * from "./sortArrayOfObjectsByField";
+export * from "./sortedCourses";
+export * from "./sortedTechnicians";
