1f196b615bd1fcef1d088a26c6aa900742f0c490
JosepConejero <bermeu@gmail.com>
2023-11-01 13:12:43 +0100

    Added lines to index.js in monthGuards folder

--- src/guardias/monthGuards/index.js
+++ src/guardias/monthGuards/index.js
@@ -2,3 +2,6 @@ export * from "./MonthBox";
 export * from "./MonthControls";
 export * from "./MonthDays";
 export * from "./Day";
+export * from "./CoursesLine";
+export * from "./SomethingBelowLine";
+export * from "./TechniciansLine";
