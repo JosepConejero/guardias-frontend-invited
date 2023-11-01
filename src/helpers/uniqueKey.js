14f8d998e534635f78c0e3db1677840a0994a1aa
JosepConejero <bermeu@gmail.com>
2023-11-01 14:30:51 +0100

    Removed comments from uniqueKey.js

--- src/helpers/uniqueKey.js
+++ src/helpers/uniqueKey.js
@@ -1,4 +1,5 @@
-//A closure to increase a numeric value without repeating it after every renderind
+//A closure to increase a numeric value without repeating it after every rendering
+
 const counter = (function () {
   let privateCounter = 0;
   function changeBy(val) {
@@ -9,10 +10,6 @@ const counter = (function () {
     increment() {
       changeBy(1);
     },
-
-    /*    decrement() {
-      changeBy(-1);
-    }, */
 
     value() {
       return privateCounter;
@@ -28,19 +25,3 @@ export const uniqueKey = () => {
     10000000
   ).toString()}-${counter.value()}`;
 };
-
-/* export const uniqueKey = () => {
-  const getRamdomInt = (max) => Math.floor(Math.random() * max);
-
-  const uniqueKey = () => {
-    counter.increment();
-    return `${new Date().getTime().toString()}-${getRamdomInt(
-      10000000
-    ).toString()}-${counter.value()}}`;
-  };
-
-  return {
-    uniqueKey,
-  };
-};
- */
