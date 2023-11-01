3b48baa6881aab730638a59f7ab34200e9f3a797
JosepConejero <bermeu@gmail.com>
2023-11-01 14:58:14 +0100

    Sorted lines in index.js in store folder

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
-export * from "./auth/authSlice";
-export * from "./month/monthSlice";
-export * from "./calendar/calendarSlice";
-export * from "./ui/uiSlice";
-export * from "./course/courseSlice";
 export * from "./appUser/appUserSlice";
+export * from "./auth/authSlice";
+export * from "./calendar/calendarSlice";
+export * from "./course/courseSlice";
 export * from "./guardDay/guardDaySlice";
+export * from "./month/monthSlice";
+export * from "./ui/uiSlice";
 
 export * from "./store";
