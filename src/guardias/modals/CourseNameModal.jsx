68e9e936a17307a90c34bb691ac1d418e3c00503
JosepConejero <bermeu@gmail.com>
2023-11-01 13:06:00 +0100

    Removed unused vars and comments from CourseNameModal.jsx

--- src/guardias/modals/CourseNameModal.jsx
+++ src/guardias/modals/CourseNameModal.jsx
@@ -23,18 +23,18 @@ const emptyCourse = {
 };
 
 export const CourseNameModal = () => {
-  const { isCourseModalOpen, closeCourseModal } = useUiStore();
+  const { closeCourseModal } = useUiStore();
   const { startSavingCourse, activeCourse, setInactiveCourse, isSaving } =
     useCoursesStore();
 
   const [formValues, setFormValues] = useState(emptyCourse);
-  const [formSubmitted, setFormSubmitted] = useState(false); //TO DO: esto lo necesitaré para controlar validaciones del formulario
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
-  const { checkedCheckbox: flc, onHandleClick: onHandleClickFlc } =
-    useCheckboxes(formValues.flc);
+  const { onHandleClick: onHandleClickFlc } = useCheckboxes(formValues.flc);
 
-  const { checkedCheckbox: frequent, onHandleClick: onHandleClickFrequent } =
-    useCheckboxes(formValues.frequent);
+  const { onHandleClick: onHandleClickFrequent } = useCheckboxes(
+    formValues.frequent
+  );
 
   const onInputChange = ({ target }) => {
     setFormValues({ ...formValues, [target.name]: target.value });
@@ -47,7 +47,6 @@ export const CourseNameModal = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
     setFormSubmitted(true);
-    //aquí haría validaciones que podrían poner el formSubmitted a false (vídeo 357 '5 más o menos)
     if (formValues.title !== "") {
       await startSavingCourse(formValues);
       onCloseModal();
@@ -56,7 +55,6 @@ export const CourseNameModal = () => {
       Swal.fire({
         title: "El nombre del curso no puede estar vacío.",
         text: "Por favor, modifica esto antes de guardar",
-        //target: document.getElementById("dialog-courses"), //target: document.getElementById('dialog'),
         target: document.getElementById("modal-fondo"),
         icon: "error",
       });
@@ -84,9 +82,6 @@ export const CourseNameModal = () => {
       ) : (
         <>
           <Grid
-            // open={isCourseModalOpen}
-            //onClose={onCloseModal}
-            //id="dialog-courses"
             sx={{
               width: { xs: "390px", md: "600px" },
             }}
@@ -100,16 +95,10 @@ export const CourseNameModal = () => {
                 container
                 direction="column"
                 sx={{
-                  //    width: { xs: "330px", md: "600px" },
                   p: { xs: "15px", md: "20px" },
-                  //      "& .MuiGrid-root": { margin: "0px", padding: "0px" },
-                  //"& .MuiGrid-container": { margin: "0px", padding: "0px" },
                 }}
               >
-                <Grid
-                  item
-                  //sm={12} sx={{ mt: 1 }}
-                >
+                <Grid item>
                   <TextField
                     label="Nombre del curso"
                     type="text"
@@ -121,10 +110,7 @@ export const CourseNameModal = () => {
                   />
                 </Grid>
 
-                <Grid
-                  item
-                  // xs={12} md={3}
-                >
+                <Grid item>
                   <FormControlLabel
                     control={
                       <Checkbox
@@ -142,14 +128,7 @@ export const CourseNameModal = () => {
                   />
                 </Grid>
 
-                <Grid
-                  item
-                  // xs={12} md={3}
-                  sx={{
-                    mb: 1,
-                    //
-                  }}
-                >
+                <Grid item sx={{ mb: 1 }}>
                   <FormControlLabel
                     control={
                       <Checkbox
