f4033ff1b4217fa7d7ec7113388acd451809d7a8
JosepConejero <bermeu@gmail.com>
2024-02-08 18:15:55 +0100

    Deleted files .js in helpers

--- src/helpers/extractItemByProperty.js
+++
@@ -1,7 +0,0 @@
-export const extractItemByProperty = (completeArray, oneProperty, value) => {
-  for (let i = 0; i < completeArray.length; i++) {
-    if (completeArray[i][`${oneProperty}`] === value)
-      return { ...completeArray[i] };
-  }
-  return {};
-};
