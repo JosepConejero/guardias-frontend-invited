36e24ccdfcf7555d4af51c7ee2066c8675117b2d
JosepConejero <bermeu@gmail.com>
2024-02-29 20:43:21 +0100

    Set {isOpen &&.... instead of if (isOpen).... in return sentence

--- src/guardias/modals/basicModal/BasicModal.tsx
+++ src/guardias/modals/basicModal/BasicModal.tsx
@@ -6,20 +6,15 @@ export const BasicModal = ({
   isOpen,
   closeModal,
   children,
-}: BasicModalProps): JSX.Element | undefined => {
+}: BasicModalProps): JSX.Element => {
   const keyPress = useCallback(
     (event: KeyboardEvent) => {
-      //(event: any) => {
       if (event.key === "Escape" && isOpen) {
         closeModal();
       }
     },
     [closeModal, isOpen]
   );
-
-  /* interface prueba extends SyntheticEvent {
-    target: EventTarget & HTMLElement;
-  } */
 
   const onHandleClick = (
     //  event: MouseEvent<HTMLElement> & { target: EventTarget & HTMLElement }
@@ -38,14 +33,18 @@ export const BasicModal = ({
     return () => document.removeEventListener("keydown", keyPress);
   }, [keyPress]);
 
-  if (isOpen)
-    return (
-      <div onMouseDown={onHandleClick} id="modal-fondo" className="darkBG">
-        <div id="modal-central" className="centered">
-          <div className="modal">
-            <div>{children}</div>
+  //if (isOpen)
+  return (
+    <>
+      {isOpen && (
+        <div onMouseDown={onHandleClick} id="modal-fondo" className="darkBG">
+          <div id="modal-central" className="centered">
+            <div className="modal">
+              <div>{children}</div>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      )}
+    </>
+  );
 };
