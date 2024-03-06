3acc6f2c9419527e4c61041b4b89335c2cc87fbd
JosepConejero <bermeu@gmail.com>
2024-03-06 20:59:08 +0100

    sortedCourses.test.ts

---
+++ src/tests/helpers/sortedCourses.test.ts
@@ -0,0 +1,85 @@
+import { sortedCourses } from "../../helpers";
+
+const fourCourses = [
+  {
+    title: "course 1",
+    frequent: false,
+    flc: false,
+    id: "id1",
+  },
+  {
+    title: "course 2",
+    frequent: true,
+    flc: false,
+    id: "id2",
+  },
+  {
+    title: "course 3",
+    frequent: false,
+    flc: true,
+    id: "id3",
+  },
+  {
+    title: "course 4",
+    frequent: true,
+    flc: true,
+    id: "id4",
+  },
+];
+
+const fourOrderedCourses = [
+  {
+    title: "course 4",
+    frequent: true,
+    flc: true,
+    id: "id4",
+  },
+  {
+    title: "course 2",
+    frequent: true,
+    flc: false,
+    id: "id2",
+  },
+  {
+    title: "course 3",
+    frequent: false,
+    flc: true,
+    id: "id3",
+  },
+  {
+    title: "course 1",
+    frequent: false,
+    flc: false,
+    id: "id1",
+  },
+];
+
+const oneCourse = [
+  {
+    title: "course 1",
+    frequent: false,
+    flc: false,
+    id: "id1",
+  },
+];
+
+describe("Given a sortedCourses function", () => {
+  describe("When it's called with an array of courses 'fourCourses'", () => {
+    test("Then it doesn't return an unordered array of 'fourCourses'", () => {
+      expect(sortedCourses(fourCourses)).not.toStrictEqual(fourCourses);
+    });
+    test("Then it returns an ordered array of 'fourCourses'", () => {
+      expect(sortedCourses(fourCourses)).toStrictEqual(fourOrderedCourses);
+    });
+  });
+  describe("When it's called with an empty array", () => {
+    test("It returns an empty array", () => {
+      expect(sortedCourses([])).toStrictEqual([]);
+    });
+  });
+  describe("When it's called with a single course", () => {
+    test("It returns the same course", () => {
+      expect(sortedCourses(oneCourse)).toStrictEqual(oneCourse);
+    });
+  });
+});
