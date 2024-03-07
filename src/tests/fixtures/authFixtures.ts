b4b90a00ba1b711c57d27eab63f482c65fe8c002
JosepConejero <bermeu@gmail.com>
2024-03-07 18:28:07 +0100

    Added test file directory structure

---
+++ src/tests/fixtures/authFixtures.ts
@@ -0,0 +1,34 @@
+/* export const initialState = {
+  status: "checking",
+  uid: null,
+  email: null,
+  displayName: null,
+  photoURL: null,
+  errorMessage: null,
+};
+
+export const authenticatedState = {
+  status: "authenticated",
+  uid: "123ABC",
+  email: "demo@google.com",
+  displayName: "Demo User",
+  photoURL: "https://demo.jpg",
+  errorMessage: null,
+};
+
+export const notAuthenticatedState = {
+  status: "not-authenticated",
+  uid: null,
+  email: null,
+  displayName: null,
+  photoURL: null,
+  errorMessage: null,
+};
+
+export const demoUser = {
+  uid: "123ABC",
+  email: "demo@google.com",
+  displayName: "Demo User",
+  photoURL: "https://demo.jpg",
+};
+ */
---
+++ src/tests/router/AppRouter.test.tsx
@@ -0,0 +1,7 @@
+describe("Given a ... function", () => {
+  describe("When it's called ...", () => {
+    test("Then it returns ...", () => {
+      expect(true).toBe(true);
+    });
+  });
+});
